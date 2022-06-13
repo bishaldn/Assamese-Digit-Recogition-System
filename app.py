40f560914f77881ce849a4d209b01cd53ecc04b7
Bishal Deep Nath <104849796+bishaldn@users.noreply.github.com>
2022-06-13 11:16:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 
 from flask import Flask, render_template, request
-import pickle
-
+import keras.models
+from keras.models import load_model
+import tensorflow
 import cv2
 import numpy as np
 
 
-model=pickle.load(open("model.pkl", "rb"))
+model= keras.models.load_model('model_filter.h5')
 
 app = Flask(__name__)
 @app.route('/', methods=['GET'])
@@ -17,35 +18,26 @@ def predict():
     imagefile=  request.files['imagefile']
     image_path= "./image/"+ imagefile.filename
     imagefile.save(image_path)
+    img=cv2.imread(image_path,cv2.IMREAD_GRAYSCALE)
+    resize_dim=32
+    img=cv2.resize(img,(resize_dim,resize_dim),interpolation=cv2.INTER_AREA) 
+    gaussian_3 = cv2.GaussianBlur(img, (9,9), 10.0) #unblur
+    img = cv2.addWeighted(img, 1.5, gaussian_3, -0.5, 0, img)
+    kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]]) #filter
+    img = cv2.filter2D(img, -1, kernel)
+    X=[]
+    X.append(img)
+    X = np.asarray(X)
+    X = X.reshape(X.shape[0],32, 32,1).astype('float32')
+    X = X/255
+    predictions_prob=model.predict(X)
+    labels=[np.argmax(pred) for pred in predictions_prob]
 
-
-    im = cv2.imread(image_path)
-    im_gray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
-    im_gray  =cv2.GaussianBlur(im_gray, (15,15), 0)
-   
-   #Threshold the image
-    ret, im_th = cv2.threshold(im_gray,100, 255, cv2.THRESH_BINARY)
-    roi = cv2.resize(im_th, (28,28), interpolation  =cv2.INTER_AREA)
-   
-    rows,cols=roi.shape
-   
-    X = []
-   
-   ## Add pixel one by one into data array
-    for i in range(rows):
-       for j in range(cols):
-           k = roi[i,j]
-           if k>100:
-               k=1
-           else:
-               k=0
-           X.append(k)
-           
-    classification  =model.predict([X])
     
-    return render_template('index.html', prediction= classification)
+    return render_template('index.html', prediction= labels)
 
 
 
 if __name__ == '__main__':
-    app.run(debug=False, port='0.0.0.0')
+    app.run(port=3000,debug=True)
+
